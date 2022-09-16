73025ed460335950e06f85bcaf7061e532853e1b
tsefat2143 <tajdidul.sefat@gmail.com>
2022-09-16 19:16:44 -0400

    add comments

--- solution_js/script.js
+++ solution_js/script.js
@@ -6,38 +6,29 @@ let lottery = () => {
 
     button.addEventListener('click', () => {
         let counter = 0
-        let value = []
         var startInterval= setInterval(() => { 
             button.style.backgroundColor='aqua'
             randomNumber = Math.floor(Math.random() * 100001); //generates a random number from 0 to 100000
-            console.log(randomNumber);
-            value.push(randomNumber)
-            userNumber.innerHTML=randomNumber
+            userNumber.innerHTML=randomNumber //updates number every iteration
             counter++
 
             if(counter==20){
-                clearInterval(startInterval)
-                setTimeout(() => {
-                    alert(userNumber.innerHTML)
+                clearInterval(startInterval) //stops setInterval
+                setTimeout(() => {//yield alert message after final number is generated
+                    if(1 < userNumber.innerHTML && userNumber.innerHTML < 1000){
+                        alert('Congratulations! You Won!')
+                    }
+                    else{
+                        alert('Too bad! You\'ve lost!')
+                    }
+                    
                 },300)
-                
+                //reset button color after number is selected
                 button.style.backgroundColor='buttonface'
                }
                
        }, 100)
-       
-       /* setTimeout(() => {
-            clearInterval(startInterval)
-            button.style.backgroundColor='buttonface'
-            if(randomNumber > 1 && randomNumber < 1000){
-                alert('You Won:'+randomNumber)
-            }
-            else{
-                alert('You Lost:'+randomNumber)
-            }
-       },1000) */
     })
-   
 }
 
 lottery()
